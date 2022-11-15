2b6b290be0a76f4609486bcb2b36edd0a324803f
ivyhy98 <ivanahy98@gmail.com>
2022-11-15 18:28:59 -0500

    Created show views of each pokemon

--- views/Show.jsx
+++ views/Show.jsx
@@ -1,1 +1,23 @@
+const React = require("react");
 
+class Show extends React.Component {
+  render() {
+    const myStyle = {
+      color: "#ffffff",
+      backgroundColor: "#000000",
+      height: "100vh",
+    };
+    const pokemon = this.props.pokemon;
+    return (
+      <div style={myStyle}>
+        <h1>Gotta Catch 'Em All</h1>
+        <h2>{pokemon.name.toUpperCase()}</h2>
+        <img src={`${pokemon.img}.jpg`} alt={pokemon.name} />
+        <br />
+        <a href={'/pokemon'}>Back to Home</a>
+      </div>
+    );
+  }
+}
+
+module.exports = Show;
